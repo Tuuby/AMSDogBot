e810a632c27a6824fbe02264d3ea436ef535f7e8
Tobias <tobiastrompell@aol.com>
2020-01-26 17:05:37 +0100

    Translate all comments to german

--- ActionBV/ActionBV.cpp
+++ ActionBV/ActionBV.cpp
@@ -13,11 +13,11 @@ double maxSpeed = 500;
 
 ArPTZ *ptz;
 
-//Constructor that initialzies the Action.
-//@border: threshold for the turning and tilting trigger according to the camera
+//Konstruktor der die Action initialisiert.
+//@border: Schwellwert für das Drehen und Tilten anhand der Kamerabilder
 //
-//Initializes the SharedMemory instance and exits the program if that fails.
-//Finally it reads X and Y from the SharedMemory and writes it to the protected variables m_X/m_Y.
+//Initialisiert eine SharedMemory Instanz und beendet das Programm, falls das fehlschlägt.
+//Abschließend werden X, Y und Visible aus dem SharedMemory gelesen und schreibt es in Variablen.
 ActionBV::ActionBV(double border) : ArAction("PioneerBV")
 {
 	m_Border = border;
@@ -37,28 +37,28 @@ ActionBV::ActionBV(double border) : ArAction("PioneerBV")
 	m_Visible = m_SharedMemory->SM_GetFloat(INDEX_VISIBLE);
 }
 
-//Function that defines the specific action that gets called by the resolver and returns it as ArActionDesired
+//Funktion, die die spezifische Action definiert, welche vom resolver aufgerufen wird und eine ArActionDesired zurückgibt.
 ArActionDesired *ActionBV::fire(ArActionDesired currentDesired)
 {
-	//Reset the ArActionDesired variable
+	//Zurücksetzen der ArActionDesired Variable
 	m_Desired.reset();
 
-	//Check if the sonar has been set and if not return an empty ArActionDesired
+	//Überprüfen ob das Sonar angelegt wurde und eine leere ArActionDesired zurückgeben falls nicht
 	if (mySonar == NULL)
 	{
 		deactivate();
 		return NULL;
 	}
 
-	//Read values from the sonar sensor and write them into the 3 different variables for later use.
+	//Abstandswerte aus dem Sonarsensor lesen und diese Werte in Variablen schreiben.
 	range = mySonar->currentReadingPolar(-45, 45) - myRobot->getRobotRadius();
 	rangeLeft = mySonar->currentReadingPolar(-180, -45);
 	rangeRight = mySonar->currentReadingPolar(45, 180);
 
-	//Calculate the velocity according to the distance in front of the robot.
-	//Range > 800: set velocity to the maxSpeed variable
-	//Range 400 - 800: set velocity to 30% percent of the range up to the maximum of maxSpeed
-	//Range < 400: set velocity to 0
+	//Geschwindigkeit anhand des Abstands zum nächsten Objekt vor dem Roboter berechnen.
+	//Range > 800: setze die Geschwindigkeit auf maxSpeed
+	//Range 400 - 800: setze die Geschwindigkeit auf 30% der range Variable mit maxSpeed als Maximum
+	//Range < 400: setze die Geschwindigkeit auf 0
 	if (range > 800)
 		m_Desired.setVel(maxSpeed);
 	else if (range > 400 && range < 800)
@@ -71,15 +71,15 @@ ArActionDesired *ActionBV::fire(ArActionDesired currentDesired)
 	else 
 		m_Desired.setVel(0);
 
-	//Calculate the velocity according to the distance on the side of the robot.
-	//Range left/right < 200: set velocity to 0 to avoid crashing
-	//Range left/right < 400: set velocity to 200 to pass objects slowly
+	//Geschwindigkeit anhand der Entfernungen links und rechts neben dem Roboter berechnen.
+	//Range left/right < 200: setze die Geschwindigkeit auf 0, um eine Kollision zu verhindern
+	//Range left/right < 400: setze die Geschwindigkeit auf 200, um langsam an nahen Objekten vorbeizufahren
 	if (rangeLeft < 200 || rangeRight < 200) 
 		m_Desired.setVel(0);
 	if (rangeLeft < 400 || rangeRight < 400) 
 		m_Desired.setVel(200);
 	
-	//If nothing red was found stand still
+	//Stehe still, wenn es keinen Rotschwerpunkt gibt
 	if (*m_Visible == 0)
 	{
 		m_Desired.setVel(0);
@@ -87,50 +87,50 @@ ArActionDesired *ActionBV::fire(ArActionDesired currentDesired)
 		return &m_Desired;
 	}
 
-	//Check if the treshold is passed
-	//and set the rotation speed according to the x coordinate.
+	//Überprüfe ob der Schwellwert erreicht wurde
+	//und setze die Rotationsgeschwindigkeit gemäß der X Koordinate.
 	if (*m_X > m_Border || *m_X < m_Border * -1)
 	{
 		m_Desired.setRotVel(*m_X / 3 * -1);
 	}
 
-	//Check if the treshold is passed
-	//and set the PTZ tilt according to the y coordinate.
+	//Überprüfe ob der Schwellwert erreicht wurde
+	//und setze den Tilt des PTZ gemäß der Y Koordinate.
 	if (*m_Y > m_Border || *m_Y < m_Border * -1)
 	{
 		ptz->tilt(*m_Y / 9 * -1);
 	}
 
-	//Return the defined ArActionDesired
+	//Gb die ArActionDesired zurück.
 	return &m_Desired;
 }
 
 int main(int argc, char** argv)
 {
-	//Define the threshold for the turning and tilting trigger according to the camera.
+	//Der Schwellwert für das Drehen und Tilten gemäß des Rotschwerpunktes im Bild
 	double border = 10;
 
-	//Initialize Aria class, ArArgumentParser, ArRobot and ArSonarDevice
+	//Initialisieren der Aria Klasse, des ArArgumentParsers, des ArRobots und des ArSonarDevice
 	Aria::init();
 	ArArgumentParser parser(&argc, argv);
 	parser.loadDefaultArguments();
 	ArRobot robot;
 	ArSonarDevice sonar;
   
-	//Instantiation of the Actions
+	//Instanziierung der Actions
 	ActionBV dogbotAction(border);
 	ArActionStallRecover recover;
     
-	//Parsing the arguments 
-	//If parsing fails the program exits
+	//Parsen der Argumente
+	//Falls das parsen fehlschlägt wird das Programm beendet
 	if(!Aria::parseArgs())
 	{
 		Aria::logOptions();
 		Aria::exit(1);
 	}
   
-	//Connect to the robot via the ArRobotConnector
-	//If it fails the program writes an error message and exits
+	//Verbindung mit dem Roboter aufbauen über den ArRobotConnector
+	//Falls das fehlschlägt wird eine Fehlermeldung geschrieben und das Programm beendet.
 	ArRobotConnector robotConnector(&parser, &robot);
 	if (!robotConnector.connectRobot())
 	{
@@ -143,33 +143,33 @@ int main(int argc, char** argv)
 		}
 	}
 
-	//Define the PTZ connector
+	//Definition des ArPTZConnectors
 	ArPTZConnector ptzConnector(&parser, &robot);
  
-	//Define keyHandler and adding them to enable ESC key to exit program
+	//Definition & Instanziierung des keyHandlers
 	ArKeyHandler keyHandler;
 	Aria::setKeyHandler(&keyHandler);
 	robot.attachKeyHandler(&keyHandler);
 	printf("You may press escape to exit\n");
 
-	//Add Actions with specific priority to the robot
+	//Hinzufügen von Actions zum Roboter mit bestimmten Priotitäten
 	robot.addAction(&recover, 100);
 	robot.addAction(&dogbotAction, 50);
 
-	//Initialize sonar sensor for the robot
+	//Initialisierung der Sonarsensoren
 	robot.addRangeDevice(&sonar);
 	mySonar = robot.findRangeDevice("sonar");
 
-	//Enabling the motors
+	//Einschalten der Motoren
 	robot.enableMotors();
 
-	//Run process loop to start the robot
+	//Starten der Prozessschleife, um den Roboter zu starten
 	robot.runAsync(true);
 	ptzConnector.connect();
 	ptz = ptzConnector.getPTZ(0);
 
 
-	//Wait for break condition
+	//Warte auf die Abbruchbedingung
 	robot.waitForRunExit();
 	Aria::exit(0);
 	return 0;
